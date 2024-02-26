1991b292e1c7388a8adbbdb911837c4cf8bd543c
Stuart Noble <stuart_noble@waters.com>
2024-02-26 14:47:27 +0000

    Modified rules.mk

--- keyboards/sofle/keymaps/spacechips/rules.mk
+++ keyboards/sofle/keymaps/spacechips/rules.mk
@@ -1,14 +1,22 @@
-SWAP_HANDS_ENABLE = no
-OLED_ENABLE = yes
+# Off to reduce binary size
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
 OS_DETECTION_ENABLE = no
+SWAP_HANDS_ENABLE = no
 WPM_ENABLE = no
+MOUSEKEY_ENABLE = no
+AUTO_SHIFT_ENABLE = no   
+RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no                       
+
+# Actively in use
+OLED_ENABLE = yes
 SPLIT_KEYBOARD = yes 
 ENCODER_ENABLE = yes 
 LTO_ENABLE = yes
 ENCODER_MAP_ENABLE = yes  
 EXTRAKEY_ENABLE = yes       
-MOUSEKEY_ENABLE = no
 CAPS_WORD_ENABLE = yes       
-AUTO_SHIFT_ENABLE = no   
-RGBLIGHT_ENABLE = no
-BACKLIGHT_ENABLE = no                       
